40e44c251fe883e3c2d743f773a3e095a48b9d08
Akash <akash.enlighten@gmail.com>
2024-03-15 01:27:58 +0530

    forgot password

---
+++ first-app/src/app/forgot-password/page.jsx
@@ -0,0 +1,9 @@
+const ForgotPassword = () => {
+    return (
+        <div>
+            <h1>Reset Password</h1>
+        </div>
+    );
+}
+
+export default ForgotPassword;
--- first-app/src/app/login/page.jsx
+++ first-app/src/app/login/page.jsx
@@ -2,6 +2,7 @@ const Login = () => {
     return (
         <div>
             <h1>Login Page</h1>
+            <a href="/forgot-password">Forgot Password?</a>
             <p>Don't have an account? <a href="/signup">Register Here</a></p>
         </div>
     );
