f14965ee1bfa8dc0f0eb3f918fdfd1d4c36b78f4
Akash <akash.enlighten@gmail.com>
2024-03-14 17:36:23 +0530

    Feed and Destinations

---
+++ first-app/src/app/destinations/page.jsx
@@ -0,0 +1,9 @@
+const Destinations = () => {
+    return (
+        <div>
+            <h1>Destinations Page</h1>
+        </div>
+    );
+}
+
+export default Destinations;
--- first-app/src/app/page.jsx
+++ first-app/src/app/page.jsx
@@ -4,7 +4,11 @@ const Home = () => {
       <a href="/login">Login</a>
       <hr></hr>
       <h1>Home Page</h1>
-      <a href="/account">Account</a>
+      <div>
+        <a href="/destinations" style={{paddingRight: '10px'}}>Destinations</a>
+        <a href="/feed" style={{paddingRight: '10px'}}>Feed</a>
+        <a href="/account">Account</a>
+      </div>
     </div>
   );
 }
